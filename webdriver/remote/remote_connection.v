c5975bba877d4f3b5b250c5bb9c2929247b7f21f
Richard Wheeler <18647491+PythonWillRule@users.noreply.github.com>
2025-01-29 12:53:40 -0500

    Build error again

--- webdriver/remote/remote_connection.v
+++ webdriver/remote/remote_connection.v
@@ -19,22 +19,25 @@ pub struct RemoteConnection {
 	client_config ?ClientConfig
 }
 
+// TODO - bug in function arg being declared mutable.
 pub fn RemoteConnection.init(remote_server_addr ?string, keep_alive ?bool, ignore_proxy ?bool, ignore_certificates ?bool,
-	init_args_for_pool_manager ?map[string]string, mut client_config ?&ClientConfig) RemoteConnection {
+	init_args_for_pool_manager ?map[string]string, client_config ?ClientConfig) RemoteConnection {
+	mut new_client_config := client_config
+	{}
 	if client_config == none {
-		client_config = ClientConfig.init(remote_server_addr, init_args_for_pool_manager)
+		new_client_config = ClientConfig.init(remote_server_addr, init_args_for_pool_manager)
 		if keep_alive != none {
-			client_config.keep_alive = keep_alive
+			new_client_config.keep_alive = keep_alive
 		}
 		if ignore_certificates != none {
-			client_config.ignore_certificates = ignore_certificates
+			new_client_config.ignore_certificates = ignore_certificates
 		}
 	}
 
 	// Keep backward compatibility for AppiumConnection - https://github.com/SeleniumHQ/selenium/issues/14694
-	// return RemoteConnection{
-	// 	timeout: client_config.timeout
-	// 	ca_certs: client_config.ca_certs
-	// 	client_config: client_config
-	// }
+	return RemoteConnection{
+		timeout:       new_client_config.timeout
+		ca_certs:      new_client_config.ca_certs
+		client_config: new_client_config
+	}
 }
